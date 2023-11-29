9bbab4fdfe273e19b7c83206dbfc935b2b3db216
jason1166 <avatarjason004@gmail.com>
2023-11-29 22:21:31 +0800

    added summon pages and feature
    Added Mahoraga Summon and Penumbral Stalker Summon pages
    
    changed default popup size
    
    change hyperlink for summon window to be image with a caption

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function basicPopup(url) {
-    popupWindow = window.open(url,'popUpWindow','height=300,width=700,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+    popupWindow = window.open(url,'popUpWindow','height=1000,width=700,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
         }
