9a5f09ed59e39c7fd19a56c97a3952d79d3800cc
jason1166 <avatarjason004@gmail.com>
2023-12-18 17:43:51 +0800

    New features and dreams page additions
    Added larger modal images for all shade images in shade data pages
    
    Added multiple images for dreams and created slideshow for them

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+//Opening PopUp Windows for Summons etc.
 function basicPopup(url) {
-    popupWindow = window.open(url,'popUpWindow','height=1000,width=1000,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
-        }
+  popupWindow = window.open(
+    url,
+    "popUpWindow",
+    "height=1000,width=1000,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes"
+  );
+}
+
+//Opening and closing Modal Images
+const modal = document.getElementById("myModal");
+
+const img = document.getElementById("shade-image");
+
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+const span = document.getElementsByClassName("close")[0];
+
+span.onclick = () => {
+  modal.style.display = "none";
+};
+
+modal.onclick = () => {
+  modal.style.display = "none";
+};
