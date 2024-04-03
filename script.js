58e0e458b8eca9eccefe68c609496e20bb42648f
jason1166 <78394632+jason1166@users.noreply.github.com>
2024-04-03 12:02:40 +0800

    Rework: Page Structure overhaul
    Experimented and added on new sidebar for menu instead of old UI

--- script.js
+++ script.js
@@ -54,3 +54,19 @@ img4.onclick = function () {
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 };
+
+
+/*Siderbar experiment*/
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("sidebarBtn").style.display = "none";
+  document.getElementById("homepage").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("sidebarBtn").style.display = "block";
+  document.getElementById("homepage").style.marginLeft = "0";
+}
